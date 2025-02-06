7c276d50da7cb7455579c72b3e8c82382344700d
Sameer <you@example.com>
2025-02-06 18:56:59 +0530

    Initial commit

---
+++ src/main/java/com/springboot/myApp/Dev.java
@@ -0,0 +1,10 @@
+package com.springboot.myApp;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build() {
+        System.out.println("Hello World!");
+    }
+
+}
